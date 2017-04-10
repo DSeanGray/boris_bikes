6d6ef6949f313c429671930f7fd78bd02fbfc0d3
david gray <davidseangray@gmail.com>
2017-04-10 17:28:15 +0100

    tdd tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,13 @@
 require '../lib/docking_station.rb'
+require '../lib/bike.rb'
 
 #In spec/docking_station.rb
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
